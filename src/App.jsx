5ed26b9ca09b7e0f87206fd399668ab238ad1daf
roy-stapp1solutions <roy@stapp1.solutions>
2024-04-26 15:28:22 +0200

    Progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function App() {
+  const [response, setResponse] = useState(null);
+  const fetchData = async () => {
+    const response = await axios.get(
+      "http://localhost:8000/sources/api/sources/1",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const data = response.data;
+    console.log(response);
+    setResponse(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
-    <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+    <h1 className="text-3xl font-bold underline text-red-500">
+      {JSON.stringify(response)}
+    </h1>
   );
 }
