a52d8fb113d7e9f98209da48ecd35e063057f6f4
roy-stapp1solutions <roy@stapp1.solutions>
2024-05-15 13:55:11 +0200

    Retrieve story details

--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -1,4 +1,43 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 export default function Story() {
   const id = window.location.pathname.split("/").pop();
-  return <>{id}</>;
+  const [story, setStory] = useState({});
+  const navigate = useNavigate();
+
+  const getSources = async () => {
+    await axios
+      .get(import.meta.env.VITE_API_URL + "/api/stories/" + id, {
+        auth: {
+          username: import.meta.env.VITE_AUTH_USERNAME,
+          password: import.meta.env.VITE_AUTH_PASSWORD,
+        },
+      })
+      .then(async function (response) {
+        setStory(response.data);
+        console.log(response.data);
+      })
+      .catch(() => {
+        console.log("Error on Authentication");
+      });
+  };
+
+  useEffect(() => {
+    getSources();
+  }, []);
+  return (
+    <>
+      <button
+        type="button"
+        className="rounded-md bg-red-600 px-2.5 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+        onClick={() => navigate(-1)}
+      >
+        Back
+      </button>
+
+      {JSON.stringify(story)}
+    </>
+  );
 }
